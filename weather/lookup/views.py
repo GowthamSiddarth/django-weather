0e100813850b13baa599069f535d8c9a02cd36e8
Gowtham Behara <gothsiddu@gmail.com>
2020-08-26 17:55:18 +0530

    Load weather data through api and render on home page

--- weather/lookup/views.py
+++ weather/lookup/views.py
@@ -3,7 +3,17 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {})
+    import requests
+
+    api_request = requests.get(
+        "http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=89129&distance=5&API_KEY=0620D809-DB96-499D-9416-B2244A45A0D3")
+    try:
+        api = api_request.json()
+    except Exception as e:
+        print(e)
+        api = "Error..."
+
+    return render(request, 'home.html', {'api': api})
 
 
 def about(request):
